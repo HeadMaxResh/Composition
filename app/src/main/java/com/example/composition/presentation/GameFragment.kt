a5f33bb7266d3158acc9962bb884f3fd3fdfbfbd
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-06-06 22:36:23 +0400

    DataBinding for GameFinishedFragment

--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -57,6 +57,8 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         observeViewModel()
         setClickListenersToOptions()
     }
